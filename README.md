0ffe3a12c14c8c21c29450dc94c8b845a224b446
Robin Lebhar <robin_lebhar@hotmail.fr>
2018-06-02 18:48:18 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 ### Dans cette branche : ###
-+ Utilisation de postman pour ajouter des utilisateurs
-+ Création d'un controller d'authentification
-+ Instanciation d'un model de user à partir des données du post
++ Hashage du mot de passe avec bcrypt ( model user.js)
--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,29 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+const bcrypt = require("bcrypt-nodejs")
 
 //Création du model
 const userSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
     password: String
 });
+
+userSchema.pre("save", function (next) {
+    const user = this;
+
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        })
+    })
+})
 
 const UserModel = mongoose.model("user", userSchema);
 
