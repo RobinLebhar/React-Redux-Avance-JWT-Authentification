fa77de67474e01a9364bcd2cf51e66ce51e43936
Robin Lebhar <robin_lebhar@hotmail.fr>
2018-08-27 19:35:36 -0400

    Delete app.js

--- src/components/app.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-require("../style.css");
-export default class App extends Component {
-  render() {
-    return (
-      "Ceci est une app créée avec react-create-app"
-    )
-  }
-}
